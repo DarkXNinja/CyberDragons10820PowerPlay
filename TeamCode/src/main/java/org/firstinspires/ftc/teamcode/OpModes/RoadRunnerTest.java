ea1b2ed27a45a9a7c6e1bbba34f594e0ca7d8955
Brendan D <79545485+b-c-d@users.noreply.github.com>
2022-10-30 15:54:59 -0400

    Update RoadRunnerTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/RoadRunnerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/RoadRunnerTest.java
@@ -4,22 +4,22 @@ public class MyOpmode extends LinearOpMode {
 
     //Makes the path for the robot to follow
     //.splineTo(new Vector2d(xCord, yCord), rotation)
-    Trajectory autoTrajectory = drive.trajectoryBuilder(new Pose2d(-6, -3, Math.toRadians(90)))
+    Trajectory autoTrajectory = drive.trajectoryBuilder(new Pose2d(-3, 6, Math.toRadians(90)))
         //Step 1: Drop off cone at low juction
-        .splineTo(new Vector2d(-4, -3), 0)
+        .splineTo(new Vector2d(-3, 4), 0)
         //Step 2: Pick up detection cone and drop it off at medium junction
-        .splineTo(new Vector2d(-4.5, -3), 90)
-        .splineTo(new Vector2d(-2, -3, 0)
+        .splineTo(new Vector2d(-3, 4.5), 90)
+        .splineTo(new Vector2d(-3, 2, 0)
         //Step 3: Pick up and drop off cones at the low junction by the cone station (x3)
-        .splineTo(new Vector2d(-1, -5), 180)
-        .splineTo(new Vector2d(-1, -4), -90)
-        .splineTo(new Vector2d(-1, -5), 180)
-        .splineTo(new Vector2d(-1, -4), -90)
-        .splineTo(new Vector2d(-1, -5), 180)
-        .splineTo(new Vector2d(-1, -4), -90)
+        .splineTo(new Vector2d(-5, 1), 180)
+        .splineTo(new Vector2d(-4, 1), -90)
+        .splineTo(new Vector2d(-5, 1), 180)
+        .splineTo(new Vector2d(-4, 1), -90)
+        .splineTo(new Vector2d(-5, 1), 180)
+        .splineTo(new Vector2d(-4, 1), -90)
         //Step 4 pick up cone and drop it off it high juntion at zone 2 and 1
-        .splineTo(new Vector2d(-1, -5), 180)
-        .splineTo(new Vector2d(-1, -2), 90)
+        .splineTo(new Vector2d(-5, 1), 180)
+        .splineTo(new Vector2d(-2, 1), 90)
         .build();
         
     waitForStart();
