b0825b75cd9713cec615555625491ea9baf09341
Aadarsh Senthil <aadhisen2006@gmail.com>
2022-10-30 16:25:55 -0400

    merging the branches, all code will now be developed on the master branch from now on

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/RoadRunnerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/RoadRunnerTest.java
@@ -1,4 +1,12 @@
-public class MyOpmode extends LinearOpMode {
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.RoadRunnerConfiguration.drive.SampleMecanumDrive;
+
+public class RoadRunnnerTest extends LinearOpMode {
+
   public void runOpMode() {
     SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
@@ -9,7 +17,7 @@ public class MyOpmode extends LinearOpMode {
         .splineTo(new Vector2d(-3, 4), 0)
         //Step 2: Pick up detection cone and drop it off at medium junction
         .splineTo(new Vector2d(-3, 4.5), 90)
-        .splineTo(new Vector2d(-3, 2, 0)
+        .splineTo(new Vector2d(-3, 2), 0)
         //Step 3: Pick up and drop off cones at the low junction by the cone station (x3)
         .splineTo(new Vector2d(-5, 1), 180)
         .splineTo(new Vector2d(-4, 1), -90)
