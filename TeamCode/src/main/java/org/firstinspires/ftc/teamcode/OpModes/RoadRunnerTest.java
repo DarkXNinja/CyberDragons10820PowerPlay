0a134d2f1668c0d1e7fe923708f907828832b9d2
harshalakam <harshalsummer17@gmail.com>
2023-01-02 15:09:20 -0500

    New Road runner test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/RoadRunnerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/RoadRunnerTest.java
@@ -1,10 +1,19 @@
 package org.firstinspires.ftc.teamcode.OpModes;
 
+import android.text.method.Touch;
+
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
@@ -48,8 +57,14 @@ public class RoadRunnerTest extends LinearOpMode {
 
   ElapsedTime timer = new ElapsedTime();
 
+  DcMotorEx lift1;
+  DcMotorEx lift2;
 
+  Servo gripper;
+  CRServo rotator;
 
+  DistanceSensor heightSensor;
+  TouchSensor centeringSensor;
 
 
   @Override
@@ -62,66 +77,53 @@ public class RoadRunnerTest extends LinearOpMode {
 
     double slowerVel = 24.0;
 
+    initializeRobot();
+
     waitForStart();
 
     if (isStopRequested()) return;
 
 
-      int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-      camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
-      aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
+    int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+    camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+    aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
 
-      camera.setPipeline(aprilTagDetectionPipeline);
-      camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
-        @Override
-        public void onOpened() {
-          camera.startStreaming(800, 448, OpenCvCameraRotation.UPRIGHT);
+    camera.setPipeline(aprilTagDetectionPipeline);
+    camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+      @Override
+      public void onOpened() {
+        camera.startStreaming(800, 448, OpenCvCameraRotation.UPRIGHT);
+      }
+
+      @Override
+      public void onError(int errorCode) {
+
+      }
+    });
+
+    timer.reset();
+    while (timer.milliseconds() < 2000) {
+
+      ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
+
+      if (currentDetections.size() != 0) {
+        boolean tagFound = false;
+
+        for (AprilTagDetection tag : currentDetections) {
+          if (tag.id == LEFT || tag.id == MIDDLE || tag.id == RIGHT) {
+            tagOfInterest = tag;
+            tagFound = true;
+            break;
+          }
         }
 
-        @Override
-        public void onError(int errorCode) {
+        if (tagFound) {
 
-        }
-      });
-
-      timer.reset();
-      while (timer.milliseconds() < 2000) {
-
-        ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
-
-        if (currentDetections.size() != 0) {
-          boolean tagFound = false;
-
-          for (AprilTagDetection tag : currentDetections) {
-            if (tag.id == LEFT || tag.id == MIDDLE || tag.id == RIGHT) {
-              tagOfInterest = tag;
-              tagFound = true;
-              break;
-            }
-          }
-
-          if (tagFound) {
-
-            telemetry.addLine("Tag of interest is in sight!\n\nLocation data:");
-            tagToTelemetry(tagOfInterest);
-
-          } else {
-
-            telemetry.addLine("Don't see tag of interest :(");
-
-            if (tagOfInterest == null) {
-
-              telemetry.addLine("(The tag has never been seen)");
-
-            } else {
-
-              telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
-              tagToTelemetry(tagOfInterest);
-
-            }
-          }
+          telemetry.addLine("Tag of interest is in sight!\n\nLocation data:");
+          tagToTelemetry(tagOfInterest);
 
         } else {
+
           telemetry.addLine("Don't see tag of interest :(");
 
           if (tagOfInterest == null) {
@@ -132,168 +134,202 @@ public class RoadRunnerTest extends LinearOpMode {
 
             telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
             tagToTelemetry(tagOfInterest);
-          }
 
+          }
         }
 
-        telemetry.update();
-        sleep(20);
-
-      }
-      /* Update the telemetry */
-      if (tagOfInterest != null) {
-
-        telemetry.addLine("Tag snapshot:\n");
-        tagToTelemetry(tagOfInterest);
-        telemetry.update();
-
       } else {
+        telemetry.addLine("Don't see tag of interest :(");
 
-        telemetry.addLine("No tag snapshot available, it was never sighted during the init loop :(");
-        telemetry.update();
+        if (tagOfInterest == null) {
+
+          telemetry.addLine("(The tag has never been seen)");
+
+        } else {
+
+          telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+          tagToTelemetry(tagOfInterest);
+        }
+
       }
 
-      //Makes the path for the robot to follow
-      //.splineTo(new Vector2d(xCord, yCord), rotation)
-      Trajectory autoTrajectory1 = drive.trajectoryBuilder(new Pose2d(-36, 72, Math.toRadians(270)))
-              //Step 1: Drop off cone at low junction
-              //.forward(24.0)
-              .lineToLinearHeading(new Pose2d(-40, 54, Math.toRadians(180)))
-              .addDisplacementMarker(() -> {
-                // Run your action in here!
-                //grabberServo.setPower(1.0);
-              })
-              .build();
-
-      Trajectory autoTrajectory2 = drive.trajectoryBuilder(autoTrajectory1.end())
-              //.lineToLinearHeading(new Pose2d(-34, 57, Math.toRadians(270)))
-              .back(6.0)
-              .build();
-
-      Trajectory autoTrajectory5 = drive.trajectoryBuilder(autoTrajectory2.end())
-              .strafeRight(3.0)
-              .build();
-
-      Trajectory autoTrajectory3 = drive.trajectoryBuilder(autoTrajectory5.end().plus(new Pose2d(0,0,Math.toRadians(90))))
-              //.lineToLinearHeading(new Pose2d(-34,58, Math.toRadians(270)))
-              .forward(9.0)
-              .addDisplacementMarker(() -> {
-                // Run your action in here!
-                //grabberServo.setPower(-1.0);
-              })
-              .build();
-
-      Trajectory autoTrajectory4 = drive.trajectoryBuilder(autoTrajectory3.end())
-              .lineToLinearHeading(new Pose2d(-36, 28, Math.toRadians(360)),
-                      SampleMecanumDrive.getVelocityConstraint(slowerVel, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
-                      SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL)
-              )
-              .addDisplacementMarker(() -> {
-                // Run your action in here!
-                //grabberServo.setPower(1.0);
-              })
-              .build();
-
-      Trajectory autoTrajectory6 = drive.trajectoryBuilder(autoTrajectory4.end().plus(new Pose2d(0,0,Math.toRadians(180))))
-              .strafeLeft(8.0)
-              .build();
-
-      Trajectory autoTrajectory7 = drive.trajectoryBuilder(autoTrajectory6.end())
-              .lineTo(new Vector2d(-66, 12))
-              .addTemporalMarker(0, () -> {
-
-                //timer.reset();
-                //linearSlide.setPower(-1.0);
-                //while  (timer.milliseconds() < 2500.0) {
-
-                //}
-                //linearSlide.setPower(0);
-
-
-              })
-              .build();
-
-      Trajectory autoTrajectory8 = drive.trajectoryBuilder(autoTrajectory7.end().plus(new Pose2d(0,0,Math.toRadians(180))))
-              .forward(24.0)
-              .build();
-
-      Trajectory autoTrajectory9 = drive.trajectoryBuilder(autoTrajectory8.end())
-              .lineToLinearHeading(new Pose2d(-36, 28, Math.toRadians(360)))
-              .build();
-
-      Trajectory autoTrajectory10 = drive.trajectoryBuilder(autoTrajectory9.end())
-              .strafeLeft(16.0)
-              .build();
-
-      drive.followTrajectory(
-              // drops cone at ground junction
-              autoTrajectory1);
-      Thread.sleep(1500);
-      drive.followTrajectory(
-              // moves backward 6 inches
-              autoTrajectory2);
-      drive.followTrajectory(
-              // strafe right 3 inches
-              autoTrajectory5);
-      drive.turn(Math.toRadians(90));
-      drive.followTrajectory(
-              // grabs detection cone
-              autoTrajectory3);
-      Thread.sleep(500);
-      //linearSlide.setPower(1.0);
-      Thread.sleep(5250);
-      //linearSlide.setPower(0);
-      //drive.turn(Math.toRadians(45));
-      drive.followTrajectory(
-              // goes to mid junction and drops cone
-              autoTrajectory4);
-      Thread.sleep(1500);
-      drive.turn(Math.toRadians(180));
-      Thread.sleep(250);
-      drive.followTrajectory(
-              // strafes left
-              autoTrajectory6);
-      drive.followTrajectory(
-              // goes to stack of cones
-              autoTrajectory7);
-      drive.turn(Math.toRadians(180));
-      drive.followTrajectory(
-              // moves forward 12 inches
-              autoTrajectory8);
-      drive.followTrajectory(
-              // goes to mid junction round 2
-              autoTrajectory9);
-      drive.followTrajectory(
-              // goes to zone 2
-              autoTrajectory10);
-
-      /* Actually do something useful */
-      if (tagOfInterest == null || tagOfInterest.id == LEFT) {
-
-        //trajectory
-        Trajectory zone1 = drive.trajectoryBuilder(new Pose2d(-36,36,Math.toRadians(360)))
-                .forward(16.0)
-                .build();
-
-        drive.followTrajectory(zone1);
-
-      } else if (tagOfInterest.id == RIGHT) {
-
-        //trajectory
-        Trajectory zone2 = drive.trajectoryBuilder(new Pose2d(-36,36,Math.toRadians(360)))
-                .back(16.0)
-                .build();
-
-        drive.followTrajectory(zone2);
-
-      } else {
-
-        // right trajectory
-      }
-
-
+      telemetry.update();
+      sleep(20);
 
     }
+    /* Update the telemetry */
+    if (tagOfInterest != null) {
+
+      telemetry.addLine("Tag snapshot:\n");
+      tagToTelemetry(tagOfInterest);
+      telemetry.update();
+
+    } else {
+
+      telemetry.addLine("No tag snapshot available, it was never sighted during the init loop :(");
+      telemetry.update();
+    }
+
+    //Makes the path for the robot to follow
+    //.splineTo(new Vector2d(xCord, yCord), rotation)
+    Trajectory autoTrajectory1 = drive.trajectoryBuilder(new Pose2d(-36, 72, Math.toRadians(270)))
+            //Step 1: Drop off cone at low junction
+            //.forward(24.0)
+            .lineToLinearHeading(new Pose2d(-40, 54, Math.toRadians(180)))
+            .addDisplacementMarker(() -> {
+              // Run your action in here!
+              //grabberServo.setPower(1.0);
+            })
+            .build();
+
+    Trajectory autoTrajectory2 = drive.trajectoryBuilder(autoTrajectory1.end())
+            //.lineToLinearHeading(new Pose2d(-34, 57, Math.toRadians(270)))
+            .back(6.0)
+            .build();
+
+    Trajectory autoTrajectory5 = drive.trajectoryBuilder(autoTrajectory2.end())
+            .strafeRight(3.0)
+            .build();
+
+    Trajectory autoTrajectory3 = drive.trajectoryBuilder(autoTrajectory5.end().plus(new Pose2d(0,0,Math.toRadians(90))))
+            //.lineToLinearHeading(new Pose2d(-34,58, Math.toRadians(270)))
+            .forward(9.0)
+            .addDisplacementMarker(() -> {
+              // Run your action in here!
+              //grabberServo.setPower(-1.0);
+            })
+            .build();
+
+    Trajectory autoTrajectory4 = drive.trajectoryBuilder(autoTrajectory3.end())
+            .lineToLinearHeading(new Pose2d(-36, 28, Math.toRadians(360)),
+                    SampleMecanumDrive.getVelocityConstraint(slowerVel, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                    SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL)
+            )
+            .addDisplacementMarker(() -> {
+              // Run your action in here!
+              //grabberServo.setPower(1.0);
+            })
+            .build();
+
+    Trajectory autoTrajectory6 = drive.trajectoryBuilder(autoTrajectory4.end().plus(new Pose2d(0,0,Math.toRadians(180))))
+            .strafeLeft(8.0)
+            .build();
+
+    Trajectory autoTrajectory7 = drive.trajectoryBuilder(autoTrajectory6.end())
+            .lineTo(new Vector2d(-66, 12))
+            .addTemporalMarker(0, () -> {
+
+              //timer.reset();
+              //linearSlide.setPower(-1.0);
+              //while  (timer.milliseconds() < 2500.0) {
+
+              //}
+              //linearSlide.setPower(0);
+
+
+            })
+            .build();
+
+    Trajectory autoTrajectory8 = drive.trajectoryBuilder(autoTrajectory7.end().plus(new Pose2d(0,0,Math.toRadians(180))))
+            .forward(24.0)
+            .build();
+
+    Trajectory autoTrajectory9 = drive.trajectoryBuilder(autoTrajectory8.end())
+            .lineToLinearHeading(new Pose2d(-36, 28, Math.toRadians(360)))
+            .build();
+
+    Trajectory autoTrajectory10 = drive.trajectoryBuilder(autoTrajectory9.end())
+            .strafeLeft(16.0)
+            .build();
+
+    // before moving to ground junction, lift the gripper slightly
+    moveLiftToPosition(-250);
+    drive.followTrajectory(
+            // drops cone at ground junction
+            autoTrajectory1);
+    //open gripper
+    gripper.setPosition(0);
+    Thread.sleep(1500);
+    drive.followTrajectory(
+            // moves backward 6 inches
+            autoTrajectory2);
+    drive.followTrajectory(
+            // strafe right 3 inches
+            autoTrajectory5);
+    drive.turn(Math.toRadians(90));
+    //moving the gripper back to original position
+    moveLiftToPosition(-50);
+    drive.followTrajectory(
+            // grabs detection cone
+            autoTrajectory3);
+    //close gripper
+    gripper.setPosition(1);
+    Thread.sleep(1500);
+    drive.followTrajectory(
+            // goes to mid junction and drops cone
+            autoTrajectory4);
+    //move gripper to medium junction height
+    moveLiftToPosition(-5150);
+    //open gripper
+    gripper.setPosition(0);
+    Thread.sleep(500);
+
+    drive.turn(Math.toRadians(180));
+    Thread.sleep(250);
+    drive.followTrajectory(
+            // strafes left
+            autoTrajectory6);
+    //move lift to height of top cone on stack
+    moveLiftToPosition(-2300);
+    drive.followTrajectory(
+            // goes to stack of cones
+            autoTrajectory7);
+    //close gripper
+    gripper.setPosition(1);
+    //move lift up as to not knock down the rest of the cones
+    //move to medium junction height
+    moveLiftToPosition(-5150);
+    drive.turn(Math.toRadians(180));
+    drive.followTrajectory(
+            // moves forward 12 inches
+            autoTrajectory8);
+    drive.followTrajectory(
+            // goes to mid junction round 2
+            autoTrajectory9);
+    //open gripper
+    gripper.setPosition(0);
+
+    drive.followTrajectory(
+            // goes to zone 2
+            autoTrajectory10);
+
+    /* Actually do something useful */
+    if (tagOfInterest == null || tagOfInterest.id == LEFT) {
+
+      //trajectory
+      Trajectory zone1 = drive.trajectoryBuilder(new Pose2d(-36,36,Math.toRadians(360)))
+              .forward(16.0)
+              .build();
+
+      drive.followTrajectory(zone1);
+
+    } else if (tagOfInterest.id == RIGHT) {
+
+      //trajectory
+      Trajectory zone2 = drive.trajectoryBuilder(new Pose2d(-36,36,Math.toRadians(360)))
+              .back(16.0)
+              .build();
+
+      drive.followTrajectory(zone2);
+
+    } else {
+
+      // right trajectory
+    }
+
+
+
+  }
 
 
   public int detect() {
@@ -394,9 +430,9 @@ public class RoadRunnerTest extends LinearOpMode {
     double slowerVelocity = 24.0;
 
    */
-      //linearSlide.setPower(-1.0);
-    //Thread.sleep(5500);
-    //linearSlide.setPower(0);
+  //linearSlide.setPower(-1.0);
+  //Thread.sleep(5500);
+  //linearSlide.setPower(0);
 
 
 
@@ -414,8 +450,66 @@ public class RoadRunnerTest extends LinearOpMode {
   }
 
 
+  void initializeRobot()
+  {
+    lift1 = hardwareMap.get(DcMotorEx.class,"lift1");
+    lift2 = hardwareMap.get(DcMotorEx.class,"lift2");
 
+    lift1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    lift2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+    lift1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    lift2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+    gripper = hardwareMap.get(Servo.class, "GrabberServo");
+    gripper.setPosition(1);
+
+    rotator = hardwareMap.get(CRServo.class, "GripperRotator");
+
+    heightSensor = hardwareMap.get(DistanceSensor.class, "gripperHeight");
+
+    centeringSensor = hardwareMap.get(TouchSensor.class, "touch");
+
+    if ( centeringSensor.isPressed() == false ) {
+      telemetry.addLine("gripper is not centered");
+    } else {
+      telemetry.addLine("Robot Initialized");
+    }
+    telemetry.update();
   }
+
+  void moveLiftToPosition(int pos) {
+    int tposition = pos;
+    final int ttolerance = 50;
+    int thigher = tposition - ttolerance;
+    int tlower = tposition + ttolerance;
+
+    lift1.setTargetPosition(tposition);
+    lift1.setTargetPositionTolerance(ttolerance);
+    lift2.setTargetPosition(tposition);
+    lift2.setTargetPositionTolerance(ttolerance);
+
+    lift1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    lift2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+    lift1.setPower(0.75);
+    lift2.setPower(0.75);
+
+    while (true) {
+      int lift1position = lift1.getCurrentPosition();
+      int lift2position = lift2.getCurrentPosition();
+
+      if ( ((lift1position < tlower) && (lift1position > thigher)) ||
+              ((lift2position < tlower) && (lift2position > thigher)) ) {
+
+        lift1.setPower(0.0);
+        lift2.setPower(0.0);
+        break;
+      }
+    }
+  }
+
+}
 
 
 
