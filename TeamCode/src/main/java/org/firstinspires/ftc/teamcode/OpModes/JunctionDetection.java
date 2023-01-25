2221f781c1643066d428adc87b2a6d7fe6817ef6
Aadarsh Senthil <aadhisen2006@gmail.com>
2023-01-25 17:45:58 -0500

    added opencv files for junction detection

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/JunctionDetection.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.OpenCV.JunctionPipeline;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+@Autonomous
+public class JunctionDetection extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        OpenCvCamera camera;
+        String webcamName = "Webcam 1";
+
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, webcamName), cameraMonitorViewId);
+
+        JunctionPipeline detector = new JunctionPipeline();
+
+        camera.setPipeline(detector);
+
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera.startStreaming(320,240, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {}
+        });
+
+        while (!isStarted()) {
+
+            // need to get bounding boxes here
+            telemetry.addData("", "");
+            telemetry.update();
+
+        }
+
+        waitForStart();
+    }
+}
