578b25be3cbb75f793f66d8fde144ffd0f6c1257
harshalakam <harshalsummer17@gmail.com>
2023-02-07 18:30:27 -0500

    Create roadrunnerTeleOpTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/roadrunnerTeleOpTest.java
@@ -0,0 +1,145 @@
+package org.firstinspires.ftc.teamcode.drive.advanced;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.acmerobotics.roadrunner.util.Angle;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDriveCancelable;
+
+/*
+ * This opmode demonstrates how one can augment driver control by following Road Runner arbitrary
+ * Road Runner trajectories at any time during teleop. This really isn't recommended at all. This is
+ * not what Trajectories are meant for. A path follower is more suited for this scenario. This
+ * sample primarily serves as a demo showcasing Road Runner's capabilities.
+ * <p>
+ * This bot starts in driver controlled mode by default. The player is able to drive the bot around
+ * like any teleop opmode. However, if one of the select buttons are pressed, the bot will switch
+ * to automatic control and run to specified location on its own.
+ * <p>
+ * If A is pressed, the bot will generate a splineTo() trajectory on the fly and follow it to
+ * targetA (x: 45, y: 45, heading: 90deg).
+ * <p>
+ * If B is pressed, the bot will generate a lineTo() trajectory on the fly and follow it to
+ * targetB (x: -15, y: 25, heading: whatever the heading is when you press B).
+ * <p>
+ * If Y is pressed, the bot will turn to face 45 degrees, no matter its position on the field.
+ * <p>
+ * Pressing X will cancel trajectory following and switch control to the driver. The bot will also
+ * cede control to the driver once trajectory following is done.
+ * <p>
+ * The following may be a little off with this method as the trajectory follower and turn
+ * function assume the bot starts at rest.
+ * <p>
+ * This sample utilizes the SampleMecanumDriveCancelable.java and TrajectorySequenceRunnerCancelable.java
+ * classes. Please ensure that these files are copied into your own project.
+ */
+@TeleOp(group = "advanced")
+public class TeleOpAugmentedDriving extends LinearOpMode {
+    // Define 2 states, drive control or automatic control
+    enum Mode {
+        DRIVER_CONTROL, //player controlling it
+        AUTOMATIC_CONTROL //Robot controlling it
+    }
+
+    Mode currentMode = Mode.DRIVER_CONTROL;
+
+    // The coordinates we want the bot to automatically go to when we press the A button
+    Vector2d targetAVector0 = new Vector2d(0, 60);
+    Vector2d targetAVector = new Vector2d(-24, 48);
+    Vector2d targetAVector2 = new Vector2d(0, 12);
+    // The heading we want the bot to end on for targetA
+    double targetAHeading = Math.toRadians(270);
+    double targetAHeading2 = Math.toRadians(360);
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Initialize custom cancelable SampleMecanumDrive class
+        // Ensure that the contents are copied over from https://github.com/NoahBres/road-runner-quickstart/blob/advanced-examples/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/advanced/SampleMecanumDriveCancelable.java
+        // and https://github.com/NoahBres/road-runner-quickstart/blob/advanced-examples/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/advanced/TrajectorySequenceRunnerCancelable.java
+        SampleMecanumDriveCancelable drive = new SampleMecanumDriveCancelable(hardwareMap);
+
+        // We want to turn off velocity control for teleop
+        // Velocity control per wheel is not necessary outside of motion profiled auto
+        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Retrieve our pose from the PoseStorage.currentPose static field
+        // See AutoTransferPose.java for further details
+        drive.setPoseEstimate(PoseStorage.currentPose);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive() && !isStopRequested()) {
+            // Update the drive class
+            drive.update();
+
+            // Read pose
+            Pose2d poseEstimate = drive.getPoseEstimate();
+
+            // Print pose to telemetry
+            telemetry.addData("mode", currentMode);
+            telemetry.addData("x", poseEstimate.getX());
+            telemetry.addData("y", poseEstimate.getY());
+            telemetry.addData("heading", poseEstimate.getHeading());
+            telemetry.update();
+
+            // We follow different logic based on whether we are in manual driver control or switch
+            // control to the automatic mode
+            switch (currentMode) {
+                case DRIVER_CONTROL:
+                    drive.setWeightedDrivePower(
+                            new Pose2d(
+                                    -gamepad1.left_stick_y,
+                                    -gamepad1.left_stick_x,
+                                    -gamepad1.right_stick_x
+                            )
+                    );
+
+                    if (gamepad1.a) {
+                        // If the A button is pressed on gamepad1, we generate a splineTo()
+                        // trajectory on the fly and follow it
+                        // We switch the state to AUTOMATIC_CONTROL
+
+                        Trajectory traj1 = drive.trajectoryBuilder(poseEstimate)
+                                .splineTo(targetAVector, targetAHeading)
+                                .build();
+                        Trajectory traj2 = drive.trajectoryBuilder(poseEstimate)
+                                .splineTo(targetAVector2, targetAHeading2)
+                                .build();
+                        Trajectory traj3 = drive.trajectoryBuilder(poseEstimate)
+                                .splineTo(targetAVector, targetAHeading)
+                                .build();
+                        Trajectory traj4 = drive.trajectoryBuilder(poseEstimate)
+                                .splineTo(targetAVector0, targetAHeading)
+                                .build();
+
+                        drive.followTrajectoryAsync(traj1);
+                        drive.followTrajectoryAsync(traj2);
+                        drive.followTrajectoryAsync(traj3);
+                        drive.followTrajectoryAsync(traj4);
+
+                        currentMode = Mode.AUTOMATIC_CONTROL;
+                    }
+                    break;
+                case AUTOMATIC_CONTROL:
+                    // If x is pressed, we break out of the automatic following
+                    if (gamepad1.x) {
+                        drive.breakFollowing();
+                        currentMode = Mode.DRIVER_CONTROL;
+                    }
+
+                    // If drive finishes its task, cede control to the driver
+                    if (!drive.isBusy()) {
+                        currentMode = Mode.DRIVER_CONTROL;
+                    }
+                    break;
+            }
+        }
+    }
+}
